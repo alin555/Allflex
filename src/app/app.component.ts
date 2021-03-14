2a529e8b4de56a50457a278876e194107cdeb4bc
Alin <alin555@gmail.com>
2021-03-14 15:46:00 +0200

    alin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,11 @@ export class AppComponent implements OnInit {
   constructor(private elRef: ElementRef) {}
 
   ngOnInit() {
-    this.elRef.nativeElement.ownerDocument.body.style.backgroundSize = (this.page == 'start' || this.page == 'overview') ? '100% 90%' : '100% 75%';
+    this.elRef.nativeElement.ownerDocument.body.style.backgroundSize = (this.page == 'start' || this.page == 'overview' ||  this.page == 'remote') ? '100% 90%' : '100% 90%';
   }
 
   changePage(page: string) {
     this.page = page;
-    this.elRef.nativeElement.ownerDocument.body.style.backgroundSize = (this.page == 'start' || this.page == 'overview') ? '100% 90%' : '100% 75%';
+    this.elRef.nativeElement.ownerDocument.body.style.backgroundSize = (this.page == 'start' || this.page == 'overview' ||  this.page == 'remote') ? '100% 90%' : '100% 90%';
   }
 }
