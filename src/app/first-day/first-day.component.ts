92427898d4852eafc8570156382464f4ca7d4465
alon zrihen <alin555@gmail.com>
2022-03-10 18:07:14 +0200

    a

--- src/app/first-day/first-day.component.ts
+++ src/app/first-day/first-day.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-day',
@@ -10,6 +10,7 @@ export class FirstDayComponent implements OnInit {
   popUp = false;
   popStart = false;
   anim = true;
+  @Input() page = 1;
 
   constructor() { }
 
